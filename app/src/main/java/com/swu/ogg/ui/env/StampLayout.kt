6cf26733e99b1bd3a9f853431e15ba197858e2b6
outsit <126140478+outsit@users.noreply.github.com>
2023-05-22 02:17:36 +0900

    StampLayout 추가

--- app/src/main/java/com/swu/ogg/ui/env/StampLayout.kt
+++ app/src/main/java/com/swu/ogg/ui/env/StampLayout.kt
@@ -1,4 +1,52 @@
 package com.swu.ogg.ui.env
 
-class StampLayout {
+import android.content.res.Resources
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.swu.ogg.R
+
+class StampLayoutFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_stamp, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
+
+        viewPager.apply {
+            clipChildren = false  // No clipping the left and right items
+            clipToPadding = false  // Show the viewpager in full width without clipping the padding
+            offscreenPageLimit = 3  // Render the left and right items
+            (getChildAt(0) as RecyclerView).overScrollMode =
+                RecyclerView.OVER_SCROLL_NEVER // Remove the scroll effect
+        }
+
+        // ...
+
+        val demoData = ArrayList<String>()
+        for (i in 1..21) {
+            demoData.add(i.toString())
+        }
+
+        viewPager.adapter = CarouselRVAdapter(demoData)
+
+        val compositePageTransformer = CompositePageTransformer()
+        compositePageTransformer.addTransformer(MarginPageTransformer((15 * Resources.getSystem().displayMetrics.density).toInt()))
+        compositePageTransformer.addTransformer { page, position ->
+            val r = 1 - kotlin.math.abs(position)
+            val alpha = if (position == 0f) 1f else 0.5f // 원하는 시작 투명도와 끝 투명도 값을 지정합니다.
+            page.alpha = alpha
+        }
+        viewPager.setPageTransformer(compositePageTransformer)
+    }
 }
