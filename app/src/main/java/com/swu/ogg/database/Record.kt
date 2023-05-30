26b67c2d72de1836cfe6445aae453600fd610bb6
Voming <68582465+Voming@users.noreply.github.com>
2023-05-30 15:45:01 +0900

    db 클래스

--- app/src/main/java/com/swu/ogg/database/Record.kt
+++ app/src/main/java/com/swu/ogg/database/Record.kt
@@ -13,23 +13,26 @@ data class Record (
 )
 
 @Entity(tableName = "activityTBL")
-class ActivityTBL(@PrimaryKey(autoGenerate = true) var aID: Int,
+class ActivityTBL(@PrimaryKey(autoGenerate = true) @ColumnInfo var aID: Int,
                   @ColumnInfo(name = "aTitle") var aTitle: String,
                   @ColumnInfo(name = "aImg") var aImg: Blob?,
-                  @ColumnInfo(name = "aDetail1") var aDetail1: String?,
-                  @ColumnInfo(name = "aDetail2") var aDetail2: String?,
-                  @ColumnInfo(name = "aDetail3") var aDetail3: String?,
-                  @ColumnInfo(name = "aDetail4") var aDetail4: String?
+                  @ColumnInfo(name = "aCo2") var aCo2: Float,
+                  @ColumnInfo(name = "aCode") var aCode: Int,
+                  @ColumnInfo(name = "aNum") var aNum: Int,
+                  @ColumnInfo(name = "aFreq") var aFreq: Int,
+                  @ColumnInfo(name = "aLimit") var aLimit: Int?,
+                  @ColumnInfo(name = "aGallery") var aGallery: Boolean?
 )
 
 @Entity(tableName = "badgeTBL")
 class BadgeTBL(@PrimaryKey(autoGenerate = true) var bID: Int,
-                  @ColumnInfo(name = "bCode") var bCode: String,
-                  @ColumnInfo(name = "bTitle") var bTitle: String?,
-                  @ColumnInfo(name = "bImg") var bImg: Blob?
+               @ColumnInfo(name = "bCode") var bCode: String?,
+               @ColumnInfo(name = "bTitle") var bTitle: String,
+               @ColumnInfo(name = "bRequire") var bRequire: String?,
+               @ColumnInfo(name = "bImg") var bImg: Blob?
 )
 
-@Entity(tableName = "co2TBL",  foreignKeys = [
+@Entity(tableName = "explanTBL",  foreignKeys = [
     ForeignKey(
         entity = ActivityTBL::class,
         parentColumns = ["aID"],
@@ -41,12 +44,8 @@ class BadgeTBL(@PrimaryKey(autoGenerate = true) var bID: Int,
         childColumns = ["aTitle"]
     )
 ])
-class Co2TBL(@PrimaryKey(autoGenerate = true) var cID: Int,
-             @ColumnInfo(name = "cCode") var cCode: String?,
-             @ColumnInfo(name = "cNum") var cNum: String?,
-             @ColumnInfo(name = "cReduce") var cReduce: String?,
-             @ColumnInfo(name = "cFreq") var cFreq: String?,
-             @ColumnInfo(name = "cLimit") var cLimit: String?
+class ExplanTBL(@PrimaryKey(autoGenerate = true) var eID: Int,
+                @ColumnInfo(name = "eDetail") var eDetail: String?,
 )
 
 @Entity(tableName = "guideTBL",  foreignKeys = [
@@ -62,9 +61,8 @@ class Co2TBL(@PrimaryKey(autoGenerate = true) var cID: Int,
     )
 ])
 class GuideTBL(@PrimaryKey(autoGenerate = true) var gID: Int,
-             @ColumnInfo(name = "gGallery") var gGallery: String,
-             @ColumnInfo(name = "gDetail") var gDetail: String?,
-             @ColumnInfo(name = "gImg") var gImg: Blob
+               @ColumnInfo(name = "gGuide") var gGallery: String,
+               @ColumnInfo(name = "gImg") var gImg: Blob?
 )
 
 @Entity(tableName = "levelTBL")
@@ -81,10 +79,28 @@ class MemberTBL(@PrimaryKey(autoGenerate = true) var mID: Int,
                 @ColumnInfo(name = "mCar") var mCar: Int?
 )
 
-@Entity(tableName = "stickerTBL")
-class StickerTBL(@PrimaryKey(autoGenerate = true) var sID: Int,
-                @ColumnInfo(name = "sTitle") var sTitle: String,
-                @ColumnInfo(name = "sDetail") var sDetail: String?,
-                @ColumnInfo(name = "sImg") var sImg: Blob?,
+@Entity(tableName = "oneoffTBL")
+class OneoffTBL(@PrimaryKey(autoGenerate = true) var oID: Int,
+                @ColumnInfo(name = "oTitle") var oTitle: String,
+                @ColumnInfo(name = "oImg") var oImg: Blob?,
+                @ColumnInfo(name = "oCo2") var oCo2: Float,
+                @ColumnInfo(name = "oCode") var oCode: String,
+                @ColumnInfo(name = "oGuide") var oGuide: String?,
+                @ColumnInfo(name = "oDetail") var oDetail: String?
 )
 
+@Entity(tableName = "particulTBL")
+class ParticulTBL(@PrimaryKey(autoGenerate = true) var pID: Int,
+                 @ColumnInfo(name = "pTitle") var pTitle: String,
+                 @ColumnInfo(name = "pImg") var pImg: Blob?,
+                 @ColumnInfo(name = "pCo2") var pCo2: Float?,
+                 @ColumnInfo(name = "pGuide") var pGuide: String?,
+                 @ColumnInfo(name = "pDetail") var pDetail: String?
+)
+
+@Entity(tableName = "stickerTBL")
+class StickerTBL(@PrimaryKey(autoGenerate = true) var sID: Int,
+                 @ColumnInfo(name = "sTitle") var sTitle: String,
+                 @ColumnInfo(name = "sDetail") var pDetail: String?,
+                 @ColumnInfo(name = "sImg") var pImg: Blob?
+)
