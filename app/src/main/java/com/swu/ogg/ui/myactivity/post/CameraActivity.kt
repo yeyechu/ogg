f0b17d0fadb74fc49263b97488b95421be8c541a
Voming <68582465+Voming@users.noreply.github.com>
2023-06-13 00:19:11 +0900

    today,post DB 인증값 업데이트 쿼리

--- app/src/main/java/com/swu/ogg/ui/myactivity/post/CameraActivity.kt
+++ app/src/main/java/com/swu/ogg/ui/myactivity/post/CameraActivity.kt
@@ -1,36 +1,32 @@
 package com.swu.ogg.ui.myactivity.post
 
 import android.Manifest
-import android.app.Activity
 import android.content.ContentValues
-import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import android.graphics.Matrix
-import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ImageButton
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.swu.ogg.database.Co2Today
 import com.swu.ogg.databinding.ActivityCameraBinding
-import java.io.File
+import com.swu.ogg.dbHelper
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -40,6 +36,15 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var binding : ActivityCameraBinding
     private var imageCapture : ImageCapture? = null
     private lateinit var cameraExecutor : ExecutorService
+
+    lateinit var dbManager : dbHelper
+    lateinit var sqlitedb : SQLiteDatabase
+    lateinit var image : ByteArray
+
+
+
+    var bitmap : Bitmap? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,11 +79,60 @@ class CameraActivity : AppCompatActivity() {
         retakeButton.setOnClickListener {
             preview.visibility = View.GONE
         }
-        postButton.setOnClickListener {
 
+        dbManager = dbHelper(this)
+        sqlitedb = dbManager.readableDatabase
+
+        postButton.setOnClickListener {
             // db 처리
             // 인증 등록 사진 db 저장 후
-            // 인증 완료 비활성화 처리 필요한 부분 구현 필요
+            var aCo2 : String
+
+            var cursor: Cursor
+            cursor = sqlitedb.rawQuery("SELECT * FROM TodayTBL WHERE aTitle = '" + extraTitle + "' ;", null)
+            while(cursor.moveToNext()) {
+                aCo2 = cursor.getString(cursor.getColumnIndexOrThrow("aCo2"))
+                val aCode = cursor.getString(cursor.getColumnIndexOrThrow("aCode")).toString()
+                val aNum = cursor.getString(cursor.getColumnIndexOrThrow("aNum"))
+                val aFreq = cursor.getString(cursor.getColumnIndexOrThrow("aFreq"))
+                val aLimit = cursor.getString(cursor.getColumnIndexOrThrow("aLimit"))
+                image = bitmapToByteArray(bitmap!!)
+                println(image)
+                //alimit이 10000인경우 처리는? -> 한번에 0으로 만들고 10000일 뒤에 재등록 가능하도록
+                //aFreq(0) 일회성, aFreq(1) 하루 1회, aFreq(2) 하루 여러번
+
+                if(aLimit.toInt() > 0){
+                    if(aFreq.toInt() == 1){
+                        // 인증 완료 비활성화 처리 필요한 부분 구현 필요(디자인 기다리는중)
+                    }
+                    else if(aFreq.toInt() == 2){
+                        // 인증 완료 비활성화 처리 필요한 부분 구현 필요(디자인 기다리는중)
+                    }
+                    val newLimit = aLimit.toInt() - 1
+                    sqlitedb.execSQL("UPDATE TodayTBL SET aGallery = '" + image + "', aLimit = " + newLimit + " WHERE aTitle = '" + extraTitle + "' ;")
+                }
+                else if(aLimit.toInt() == 10000){
+                    sqlitedb.execSQL("UPDATE TodayTBL SET aGallery = '" + image + "', aLimit = " + 0 + " WHERE aTitle = '" + extraTitle + "' ;")
+                }
+
+                //post 테이블에도 구현
+                //사용자 아이디는 일단 1으로 해놓고 함
+                var cursor: Cursor
+                cursor = sqlitedb.rawQuery("SELECT * FROM post WHERE pID = '1';", null)
+                while(cursor.moveToNext()) {
+                    val pCo2Today = cursor.getString(cursor.getColumnIndexOrThrow("pCo2Today"))
+                    val pCo2All = cursor.getString(cursor.getColumnIndexOrThrow("pCo2All"))
+
+                    var newToday = pCo2Today.toFloat() + aCo2.toFloat()
+                    var newAll = pCo2All.toFloat() + aCo2.toFloat()
+
+                    sqlitedb.execSQL("UPDATE post SET pCo2Today = '" + newToday + "', pCo2All = " + newAll + " WHERE pID = 1 ;")
+                }
+
+                sqlitedb.close()
+                dbManager.close()
+            }
+
 
             // LiveData observe 구현 필요
             // -> 오늘 게이지
@@ -120,13 +174,19 @@ class CameraActivity : AppCompatActivity() {
 
     }
 
+    //비트맵 -> 바이트 변환
+    fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
+        var outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, outputStream)
+        return outputStream.toByteArray()
+    }
     // 사진 찍기
     private fun takePhoto() {
 
         // imageCapture 사용 사례에 대한 참조 불러오기 -> null이면 함수 종료
         val imageCapture = imageCapture ?: return
 
-        var bitmap : Bitmap? = null
+
 
         val name = SimpleDateFormat(FILENAME_FORMAT, Locale.KOREA).format(System.currentTimeMillis())
 
